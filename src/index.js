041f57d340dda6532260777515ea9cd36e033221
Mosby192 <545610830@qq.com>
2019-01-17 23:08:13 +0800

    mobx一般使用进阶

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import {observable} from "mobx";
+import {observable, isArrayLike, computed, action, runInAction, autorun, when, reaction} from "mobx";
 
-let num = observable.box(20);
-let str = observable.box('hello');
-let bool = observable.box(true);
+class Store {
+    @observable array = [];
+    @observable obj = {};
+    @observable map = new Map();
 
-// 使用set()设置数值
-num.set(50);
+    @observable string = 'hello';
+    @observable number = 20;
+    @observable bool = false;
 
-// get()获取原生数值
-console.log(num.get(), str, bool);
+    @action bar() {
+        store.string = 'word';
+        store.number = 333;
+        store.bool = true;
+    }
+
+}
+
+let store = new Store();
+
+// reaction(() => [store.string, store.number, store.bool], arr => console.log(arr.join('+'))); //初始化不会先运行一次
+autorun(() => console.log(store.string + store.number + store.bool)); //初始化先运行一次
+
+store.bar();
+
+// 打印结果
+//word+333+true
